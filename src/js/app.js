f9fd098649a517771536077af45e9aa43afbdaf0
chrynx <ralphmadriaga@gmail.com>
2017-08-28 19:10:37 +0100

    made the buttons work just need to fix the reset button

--- src/js/app.js
+++ src/js/app.js
@@ -1,61 +1,43 @@
 $(() => {
   // ==================VARIABLES========================
-  const $body = $('body');
-  const $header = $('header');
-  const $h1 = $('h1');
-  const $main = $('main');
-  const $game = $('.game');
-  const $topSpacer = $('.topSpacer');
-  const $leftPlayerName = $('.leftPlayerName');
-  const $topSpacerMiddleFiller = $('.topSpacerMiddleFiller');
-  const $rightPlayerName = $('.rightPlayerName');
-  const $centerArea = $('.centerArea');
-  const $leftFiller = $('.leftFiller');
-  const $leftCharacter = $('.leftCharacter');
   const $leftPhoto = $('.leftPhoto');
-  const $leftName = $('.leftName');
-  const $leftStats = $('.leftStats');
-  const $leftHP = $('.leftHP');
-  const $leftHPValue = $('.leftHPValue');
-  const $leftAV = $('.leftAV');
-  const $leftAVValue = $('.leftAVValue');
-  const $leftDV = $('.leftDV');
-  const $leftDVValue = $('.leftDVValue');
-  const $leftButtons = $('.leftButtons');
+  let $leftHPValue = parseInt($('.leftHPValue').text());
+  let $leftAVValue = parseInt($('.leftAVValue').text());
+  const $leftDVValue = parseInt($('.leftDVValue').text());
+  // const $leftButtons = $('.leftButtons');
   const $leftAttackButton = $('.leftAttackButton');
   const $leftChargeButton = $('.leftChargeButton');
   const $leftEscapeButton = $('.leftEscapeButton');
-  const $middleArea = $('.middleArea');
-  const $leftHPCalculate = $('.leftHPCalculate');
-  let $leftHPBar = $('.leftHPBar');
-  let $leftAttackBar = $('.leftAttackBar');
-  let $leftDefBar = $('.leftDefBar');
-  const $middleFiller = $('.middleFiller');
-  let $rightHPBar = $('.rightHPBar');
-  let $rightAttackBar = $('.rightAttackBar');
-  let $rightDefBar = $('.rightDefBar');
-  const $rightCharacter = $('.rightCharacter');
+  const $leftHPBar = $('.leftHPBar');
+  const $leftAttackBar = $('.leftAttackBar');
+  const $leftDefBar = $('.leftDefBar');
+  const $rightHPBar = $('.rightHPBar');
+  const $rightAttackBar = $('.rightAttackBar');
+  const $rightDefBar = $('.rightDefBar');
   const $rightPhoto = $('.rightPhoto');
-  const $rightName = $('.rightName');
-  const $rightStats = $('.rightStats');
-  const $rightHP = $('.rightHP');
-  const $rightHPValue = $('.rightHPValue');
-  const $rightAV = $('.rightAV');
-  const $rightAVValue = $('.rightAVValue');
-  const $rightDV = $('.rightDV');
-  const $rightDVValue = $('.rightDVValue');
-  const $rightButtons = $('.rightButtons');
+  let $rightHPValue = parseInt($('.rightHPValue').text());
+  let $rightAVValue = parseInt($('.rightAVValue').text());
+  const $rightDVValue = parseInt($('.rightDVValue').text());
+  // const $rightButtons = $('.rightButtons');
   const $rightAttackButton = $('.rightAttackButton');
   const $rightChargeButton = $('.rightChargeButton');
   const $rightEscapeButton = $('.rightEscapeButton');
-  const $rightFiller = $('.rightFiller');
-  const $bottomSpacer = $('.bottomSpacer');
-  const $bottomSpacerLeft = $('.bottomSpacerLeft');
-  const $bottomSpacerLeftChar = $('.bottomSpacerLeftChar');
-  const $bottomSpacerMiddle = $('.bottomSpacerMiddle');
-  const $bottomSpacerRightChar = $('.bottomSpacerRightChar');
-  const $bottomSpacerRight = $('.bottomSpacerRight');
-  const $footer = $('.footer');
+  // const $rightFiller = $('.rightFiller');
+  // const $bottomSpacer = $('.bottomSpacer');
+  // const $bottomSpacerLeft = $('.bottomSpacerLeft');
+  // const $bottomSpacerLeftChar = $('.bottomSpacerLeftChar');
+  // const $bottomSpacerMiddle = $('.bottomSpacerMiddle');
+  // const $bottomSpacerRightChar = $('.bottomSpacerRightChar');
+  // const $bottomSpacerRight = $('.bottomSpacerRight');
+  // const $footer = $('.footer');
+  const $resetButton = $('.resetButton');
+  // ==================================================
+  const $leftHPWidth = $leftHPValue * 45;
+  const $leftAttackWidth = $leftAVValue * 45;
+  const $leftDefWidth = $leftDVValue * 45;
+  const $rightHPWidth = $rightHPValue * 45;
+  const $rightAttackWidth = $rightAVValue * 45;
+  const $rightDefWidth = $rightDVValue * 45;
   // ===================FUNCTIONS=======================
   function disableLeftButtons() {
     $leftAttackButton.prop('disabled', true);
@@ -73,46 +55,121 @@ $(() => {
     $leftChargeButton.prop('disabled', false);
     $leftEscapeButton.prop('disabled', false);
   }
+  function disableButtons() {
+    $rightAttackButton.prop('disabled', true);
+    $rightChargeButton.prop('disabled', true);
+    $rightEscapeButton.prop('disabled', true);
+    $leftAttackButton.prop('disabled', true);
+    $leftChargeButton.prop('disabled', true);
+    $leftEscapeButton.prop('disabled', true);
+  }
   // =======================ACTIONS=====================
   disableRightButtons();
+  $leftHPBar.css('width', `${$leftHPWidth}px`);
+  $leftAttackBar.css('width', `${$leftAttackWidth}px`);
+  $leftDefBar.css('width', `${$leftDefWidth}px`);
+  $rightHPBar.css('width', `${$rightHPWidth}px`);
+  $rightAttackBar.css('width', `${$rightAttackWidth}px`);
+  $rightDefBar.css('width', `${$rightDefWidth}px`);
 
   // ====================LEFT BUTTONS=====================
   $leftAttackButton.on('click', () => {
     console.log('left attack');
     disableLeftButtons();
+    $rightHPValue = $rightHPValue - ($leftAVValue - $rightDVValue);
+    const remainingHP = $rightHPValue * 45;
+    $rightHPBar.css('width', `${remainingHP}px`);
+    if($rightHPValue <= 0){
+      $rightHPBar.css('width', '450px');
+      $rightHPBar.css('background-color', 'black');
+      $rightHPBar.css('border', 'none');
+      $rightPhoto.css('background', 'url(\'../src/images/you-lose.jpg\')');
+      disableButtons();
+      rightCharLost();
+    }
   });
+  function rightCharLost() {
+    $rightHPBar.css('width', '450px');
+    $rightHPBar.css('background-color', 'black');
+    $rightHPBar.css('border', 'none');
+  }
 
   $leftChargeButton.on('click', () => {
     console.log('left charge');
     disableLeftButtons();
+    $leftAVValue = $leftAVValue * 2;
+    if($leftAVValue > 10){
+      $leftAVValue = 10;
+      const doubleAttack = $leftAVValue * 45;
+      $leftAttackBar.css('width', `${doubleAttack}px`);
+    } else {
+      const doubleAttack = $leftAVValue * 45;
+      $leftAttackBar.css('width', `${doubleAttack}px`);
+    }
   });
 
   $leftEscapeButton.on('click', () => {
     console.log('left escape');
-    disableLeftButtons();
+    disableButtons();
+    $leftPhoto.css('background-image', 'url(\'../src/images/you-lose.jpg\')');
   });
 
   // ==================RIGHT BUTTONS====================
   $rightAttackButton.on('click', () => {
     console.log('right attack');
     disableRightButtons();
+    $leftHPValue = $leftHPValue - ($rightAVValue - $leftDVValue);
+    const remainingHP = $leftHPValue * 45;
+    $leftHPBar.css('width', `${remainingHP}px`);
+    if($leftHPValue <= 0){
+      leftCharLost();
+      disableButtons();
+    }
   });
-
+  function leftCharLost() {
+    $leftHPBar.css('width', '450px');
+    $leftHPBar.css('background-color', 'black');
+    $leftHPBar.css('border', 'none');
+  }
   $rightChargeButton.on('click', () => {
     console.log('right charge');
     disableRightButtons();
+    $rightAVValue = $rightAVValue * 2;
+    if($rightAVValue > 10){
+      $rightAVValue = 10;
+      const doubleAttack = $rightAVValue * 45;
+      $rightAttackBar.css('width', `${doubleAttack}px`);
+    } else {
+      const doubleAttack = $rightAVValue * 45;
+      $rightAttackBar.css('width', `${doubleAttack}px`);
+    }
   });
 
   $rightEscapeButton.on('click', () => {
     console.log('right escape');
-    disableRightButtons();
+    disableButtons();
+    $leftPhoto.css('background-image', 'url(\'../src/images/you-lose.jpg\')');
   });
+  function resetAll() {
+    $leftHPBar.css('background-color', 'green');
+    $leftHPBar.css('border', '5px solid white');
+    $rightHPBar.css('background-color', 'green');
+    $rightHPBar.css('border', '5px solid white');
+    disableRightButtons();
+    $leftHPValue = $('.leftHPValue').text();
+    $leftAVValue = $('.leftAVValue').text();
+    $rightHPValue = $('.rightHPValue').text();
+    $rightAVValue = $('.rightAVValue').text();
 
+    $leftHPBar.css('width', `${$leftHPWidth}px`);
+    $leftAttackBar.css('width', `${$leftAttackWidth}px`);
+    $leftDefBar.css('width', `${$leftDefWidth}px`);
+    $rightHPBar.css('width', `${$rightHPWidth}px`);
+    $rightAttackBar.css('width', `${$rightAttackWidth}px`);
+    $rightDefBar.css('width', `${$rightDefWidth}px`);
 
-
-
-
-
+  }
+  $resetButton.on('click', resetAll);
 
 
 
